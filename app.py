b5097e689453e3be0f62e6f3d40bfc0633b8385c
nickersonaj <nickersona@uthscsa.edu>
2024-10-09 10:23:07 -0500

    Adding comments

--- app.py
+++ app.py
@@ -1,54 +1,71 @@
+#import Controllers, configs, and libraries
 from controllers.UserAuthController import UserAuthController
 from controllers.TradingCardsController import TradingCardsController
 from controllers.ShoppingCartsController import ShoppingCartsController
 from flask import Flask, jsonify, render_template, redirect, request
 from configs.config import get_secret_key
 
+# set the path where images will be uploaded
 UPLOAD_FOLDER = 'static/images'
 
+# initiliaze the application
+# get the secret key from .env
+# and set the path for image uploads
 app = Flask(__name__)
 app.config.update(SECRET_KEY=get_secret_key())
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# initialize Controllers
 userAuth = UserAuthController()
 tradingCards = TradingCardsController()
 shoppingCarts = ShoppingCartsController()
+
+# Route to login user
 @app.route('/', methods=['GET', 'POST'])
 def login():
     return userAuth.login(request)
 
+# Log user out of system
 @app.route('/logout')
 def logout():
     return userAuth.logout()
 
+# Register user to application and save information to database
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     return userAuth.register(request)
 
+# Go to home page if user is logged in. If not then redirect to login page
 @app.route('/home', methods=['GET', 'POST'])
 def home():
     return userAuth.is_logged_in(request)
 
+# Page for uploading card. If its POST request then that means user is submitting form.
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     return tradingCards.upload(request, app)
 
+# Page for viewing all submitted cards.
 @app.route('/shop')
 def shop():
     return tradingCards.get_cards_from_shop(request)
 
+# Page to view users shopping cart.
 @app.route('/shopping_cart')
 def shopping_cart():
     return shoppingCarts.get_shopping_cart(request)
 
+# POST request to add card to shopping cart.
 @app.route('/add_to_cart/<email>/<card_id>', methods=['POST'])
 def add_to_cart(email, card_id):
     return shoppingCarts.update_cart(email, card_id, True)
 
+# POST request to remove card from shopping cart
 @app.route('/remove_item_from_cart/<email>/<card_id>', methods=['POST'])
 def remove_item_from_cart(email, card_id):
     return shoppingCarts.update_cart(email, card_id, False)
 
+# Post request to delete card entirely
 @app.route('/remove_card_from_shop/<email>/<card_id>', methods=['POST'])
 def remove_card_from_shop(email, card_id):
     return tradingCards.remove_card_from_shop(email, card_id)
--- init-db.js
+++ init-db.js
@@ -1,32 +1,5 @@
+// Drop existing tables when initializing application
 db = db.getSiblingDB("trading_card_shop_db");
-//db.trading_card_shop_tb.drop();
 db.users.drop();
 db.shopping_cart_items.drop();
 db.trading_cards.drop();
-
-/**db.users.insertMany([
-    {
-        "id": 1,
-        "email": "aubreynickerson@gmail.com",
-        "password": "letmein",
-        "date_registered": "2024-10-06"
-    }
-])
-
-db.trading_card_shop_tb.insertMany([
-    {
-        "id": 1,
-        "name": "Lion",
-        "type": "wild"
-    },
-    {
-        "id": 2,
-        "name": "Cow",
-        "type": "domestic"
-    },
-    {
-        "id": 3,
-        "name": "Tiger",
-        "type": "wild"
-    },
-]);*/
